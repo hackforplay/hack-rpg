235646ff9b69adb86d2712dd9474962323162202
teramotodaiki <i101323@gmail.com>
2017-08-15 09:42:34 +0900

    cp extra.js

---
+++ src/en/stages/extra.js
@@ -0,0 +1,49 @@
+/*
+ * 〜　コードの世界(せかい)　〜
+ * 転移装置(てんいそうち) はコードによって 封印(ふういん) されていた…
+ * これでは ステージを 改造(かいぞう) しに行けない！
+ * 下のコードによって、封印をとめられるらしいが…
+ *
+ * 『封印 を とめるには、
+ * 　どうすればいいだろうか？』
+ *
+ */
+
+export const じゅもん = ('▼ ふういん', 'する');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const flag = じゅもん !== 'する';
+
+export default function makeMagic(x, y, map, fileName) {
+	const magic = new RPGObject();
+	magic.locate(x, y, map);
+	magic.layer = RPGMap.Layer.Under;
+	magic.collisionFlag = false;
+	if (flag) {
+		magic.mod(('▼ スキン', _mまほうじんひかった));
+		magic.onのった = () => {
+			if (confirm('このステージを 改造(かいぞう) しますか？')) {
+				feeles.openEditor(fileName);
+			} else if (confirm('もどりますか？')) {
+				feeles.replace('stages/7/index.html');
+			}
+		};
+	} else {
+		magic.mod(('▼ スキン', _mまほうじん));
+		magic.onのった = () => {
+			Hack.log('ふういんのせいで つかえない');
+		};
+	}
+};
