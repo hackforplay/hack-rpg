59c9bf9cffb56e15ba91fe32e522ee001774d928
teramotodaiki <i101323@gmail.com>
2017-12-22 14:42:49 +0900

    offline plugin

--- entry.js
+++ entry.js
@@ -1,4 +1,10 @@
+// Support hot reloading
 if (process.env.NODE_ENV !== 'production') {
 	// In development, it loads JSON file and supports WDS's hot reloading
 	fetch('hack-rpg.json');
+}
+
+if (process.env.NODE_ENV === 'production') {
+	// Offline plugin
+	require('offline-plugin/runtime').install();
 }
